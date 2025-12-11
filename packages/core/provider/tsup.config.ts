3dd8fecad0ebec7a0239b12d3893ae885ae0cec5
Matheus Bastani <matheusbastani4@gmail.com>
2025-12-11 15:10:25 -0300

    chore(@nsui/provider): add tsup config

---
+++ packages/core/provider/tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts', '!src/scripts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  clean: true,
+  sourcemap: true,
+  target: 'esnext'
+})
